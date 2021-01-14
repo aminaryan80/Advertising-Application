82cc6d87402cdef4506cc631748335516deac18d
aminaryan80 <aryanmohammadamin@gmail.com>
2021-01-14 15:33:58 +0330

    fix setter and getter for advertiser field in Ad class.

--- advertising_app/model/ad.py
+++ advertising_app/model/ad.py
@@ -8,7 +8,7 @@ class Ad(BaseAdvertising):
         self.title = title
         self.img_url = img_url
         self.link = link
-        self.advertiser = advertiser
+        self._advertiser = advertiser
 
     def inc_clicks(self):
         super(Ad, self).inc_clicks()
@@ -18,8 +18,13 @@ class Ad(BaseAdvertising):
         super(Ad, self).inc_views()
         self.advertiser.inc_views()
 
+    @property
+    def advertiser(self):
+        return self._advertiser
+
+    @advertiser.setter
     def advertiser(self, advertiser):
-        self.advertiser = advertiser
+        self._advertiser = advertiser
 
     def describe_me(self):
         print("This is Ad #" + str(self.id))
